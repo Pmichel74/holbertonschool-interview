b7de92351829bbd230bc16a90f30a479b8b4c306
Your Name <you@example.com>
2025-08-07 09:39:09 +0200

    task lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that checks if all boxes can be opened."""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened using available keys
+    
+    Args:
+        boxes (list): List of lists where boxes[i] contains keys for other boxes
+    
+    Returns:
+        bool: True if all boxes can be opened, False otherwise
+    """
+    opened = [False] * len(boxes)
+    opened[0] = True
+    keys = [0]
+    
+    while keys:
+        current = keys.pop(0)
+        
+        for key in boxes[current]:
+            if 0 <= key < len(boxes) and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+    
+    return all(opened)
