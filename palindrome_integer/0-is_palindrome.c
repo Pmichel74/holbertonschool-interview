47ac56961f82c8b329c4f7277646c6ea01dd63dd
Your Name <you@example.com>
2025-10-10 12:55:48 +0200

    palindrome_integer

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - checks whether or not a given unsigned integer
+ *                 is a palindrome
+ * @n: the number to be checked
+ *
+ * Return: 1 if n is a palindrome, and 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long original = n;
+	unsigned long reversed = 0;
+
+	/* Reverse the number */
+	while (n > 0)
+	{
+		reversed = reversed * 10 + (n % 10);
+		n = n / 10;
+	}
+
+	/* Compare original with reversed */
+	if (original == reversed)
+		return (1);
+	else
+		return (0);
+}
