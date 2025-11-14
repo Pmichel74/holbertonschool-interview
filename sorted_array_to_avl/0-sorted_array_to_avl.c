7e62f72647ca1d9bfa4305cacbb673357158caf4
Your Name <you@example.com>
2025-11-14 14:27:46 +0100

    task0

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -10,18 +10,18 @@
  */
 avl_t *create_node(avl_t *parent, int value)
 {
-    avl_t *new_node;
+	avl_t *new_node;
 
-    new_node = malloc(sizeof(avl_t));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(avl_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    return (new_node);
+	return (new_node);
 }
 
 /**
@@ -35,22 +35,22 @@ avl_t *create_node(avl_t *parent, int value)
  */
 avl_t *build_avl(int *array, int start, int end, avl_t *parent)
 {
-    avl_t *node;
-    int mid;
+	avl_t *node;
+	int mid;
 
-    if (start > end)
-        return (NULL);
+	if (start > end)
+		return (NULL);
 
-    mid = start + (end - start) / 2;
+	mid = start + (end - start) / 2;
 
-    node = create_node(parent, array[mid]);
-    if (!node)
-        return (NULL);
+	node = create_node(parent, array[mid]);
+	if (!node)
+		return (NULL);
 
-    node->left = build_avl(array, start, mid - 1, node);
-    node->right = build_avl(array, mid + 1, end, node);
+	node->left = build_avl(array, start, mid - 1, node);
+	node->right = build_avl(array, mid + 1, end, node);
 
-    return (node);
+	return (node);
 }
 
 /**
@@ -62,8 +62,8 @@ avl_t *build_avl(int *array, int start, int end, avl_t *parent)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (!array || size == 0)
-        return (NULL);
+	if (!array || size == 0)
+		return (NULL);
 
-    return (build_avl(array, 0, size - 1, NULL));
+	return (build_avl(array, 0, size - 1, NULL));
 }
