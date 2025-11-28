1fdb20df3886861a904a9f2976b4aa5ce8ad4bb9
Your Name <you@example.com>
2025-11-28 13:11:52 +0100

    task0

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -49,16 +49,68 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 			prev_express->index, express_node->index);
 	}
 
-	/* Linear search in the identified range */
-	while (prev_express != NULL && prev_express->index <= express_node->index)
+
+	/**
+	 * linear_search_range - Effectue une recherche linéaire dans une plage de skip list
+	 * @start: noeud de début
+	 * @end: noeud de fin
+	 * @value: valeur à chercher
+	 *
+	 * Return: pointeur vers le noeud trouvé ou NULL
+	 */
+	static skiplist_t *linear_search_range(skiplist_t *start, skiplist_t *end, int value)
 	{
-		printf("Value checked at index [%lu] = [%d]\n",
-			prev_express->index, prev_express->n);
+		while (start != NULL && start->index <= end->index)
+		{
+			printf("Value checked at index [%lu] = [%d]\n",
+				start->index, start->n);
+			if (start->n == value)
+				return (start);
+			start = start->next;
+		}
+		return (NULL);
+	}
 
-		if (prev_express->n == value)
-			return (prev_express);
+	skiplist_t *linear_skip(skiplist_t *list, int value)
+	{
+		skiplist_t *express_node, *prev_express;
 
-		prev_express = prev_express->next;
+		if (list == NULL)
+			return (NULL);
+
+		express_node = list;
+		prev_express = list;
+
+		/* Use express lane to find the range */
+		while (express_node->express != NULL)
+		{
+			prev_express = express_node;
+			express_node = express_node->express;
+			printf("Value checked at index [%lu] = [%d]\n",
+				express_node->index, express_node->n);
+
+			if (express_node->n >= value)
+			{
+				printf("Value found between indexes [%lu] and [%lu]\n",
+					prev_express->index, express_node->index);
+				break;
+			}
+		}
+
+		/* If we've reached the end of express lane, search from last express to end */
+		if (express_node->express == NULL && express_node->n < value)
+		{
+			/* Find the actual last node */
+			prev_express = express_node;
+			while (express_node->next != NULL)
+				express_node = express_node->next;
+
+			printf("Value found between indexes [%lu] and [%lu]\n",
+				prev_express->index, express_node->index);
+		}
+
+		/* Linear search in the identified range */
+		return linear_search_range(prev_express, express_node, value);
 	}
 
 	return (NULL);
