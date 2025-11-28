2ab1ec5ab4eeaba6646b9a2ac90014c101b28569
Your Name <you@example.com>
2025-11-28 13:09:41 +0100

    task0

--- linear_skip/create_skiplist.c
+++ linear_skip/create_skiplist.c
@@ -4,9 +4,11 @@
 
 /**
  * init_express - Initializes the express lane of the linked list
- *
  * @list: Pointer to the head node of the list
  * @size: Number of nodes in the list
+ *
+ * Description: Sets the express pointer every sqrt(size) nodes.
+ * Allows faster search in the skip list.
  */
 void init_express(skiplist_t *list, size_t size)
 {
@@ -25,21 +27,21 @@ void init_express(skiplist_t *list, size_t size)
 }
 
 /**
- * create_skiplist - Create a single linked list
- *
+ * create_skiplist - Creates a single linked list (skip list)
  * @array: Pointer to the array used to fill the list
  * @size: Size of the array
  *
  * Return: A pointer to the head of the created list (NULL on failure)
+ *
+ * Description: Allocates nodes for each array element, links them,
+ * and initializes the express lanes for fast traversal.
  */
 skiplist_t *create_skiplist(int *array, size_t size)
 {
-	skiplist_t *list;
-	skiplist_t *node;
-	size_t save_size;
+	skiplist_t *list = NULL;
+	skiplist_t *node = NULL;
+	size_t save_size = size;
 
-	list = NULL;
-	save_size = size;
 	while (array && size--)
 	{
 		node = malloc(sizeof(*node));
